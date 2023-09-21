695f6cac449ca6fb487617e044a41467a52907e7
Anjali Malviya <108400908+ANJALI-1233@users.noreply.github.com>
2023-09-21 23:51:07 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => (
     <div className="wrapper">
-        <h2>This is new about page !!!</h2>
+        <h2>This is the about page</h2>
     </div>
 );
 
