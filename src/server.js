f71700c84bf06780c31a234d725418cf9d338c82
Anjali Malviya <108400908+ANJALI-1233@users.noreply.github.com>
2023-09-21 15:02:34 +0530

    remove space

--- src/server.js
+++ src/server.js
@@ -8,7 +8,6 @@ import { StaticRouter, matchPath } from "react-router-dom";
 import { Provider as ReduxProvider } from "react-redux";
 import Helmet from "react-helmet";
 import routes from "./routes";
-
 import Layout from "./components/Layout";
 import createStore, { initializeSession } from "./store";
 
